b36f9c2a64236a7b43e425de877254a83711b80e
mayhemphone <mayhemphone@gmail.com>
2019-09-06 12:38:31 -0700

    CORS fixes, ready to redeploy

--- hackaton_server/settings.py
+++ hackaton_server/settings.py
@@ -1,3 +1,6 @@
+# CORS INFO
+# https://github.com/adamchainz/django-cors-headers
+
 """
 Django settings for hackaton_server project.
 
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'hackaton_server.urls'
@@ -128,3 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_WHITELIST = [
+	'http://localhost:3000',
+	'https://worth-it-client.herokuapp.com/'
+]
+# enable all domains DANGER!!
+# CORS_ORIGIN_ALLOW_ALL = True
+
