05283efd83dbf4f3267d72afad8d40a1bf5a7295
mayhemphone <mayhemphone@gmail.com>
2019-09-06 12:43:06 -0700

    needed a push

--- hackaton_server/settings.py
+++ hackaton_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
+
 """
 
 import os
